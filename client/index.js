bda5eb0ad789458ba87b72d5b8c15cfb1ca3d37b
niwatoro <girlwithskyhairpin@gmail.com>
2023-11-12 16:01:01 +0900

    Add client proof verification logic

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: "Norman Block",
+    proof: new MerkleTree(niceList).getProof(niceList.findIndex((n) => n === "Norman Block")),
   });
 
   console.log({ gift });
